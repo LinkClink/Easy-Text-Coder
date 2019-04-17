50a887470c4022bcc1c3cac0fa4b9843457e9ae8
LinkClink <shekaqwe@gmail.com>
2019-04-17 19:18:45 +0200

    Include save file function

--- app/src/main/java/com/example/firstteam/etcodedecoder/Coding.java
+++ app/src/main/java/com/example/firstteam/etcodedecoder/Coding.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +25,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,25 +36,29 @@ public class Coding extends AppCompatActivity
     private Button savetxt;
     private EditText edit_text;
 
-    Uri op_file_uri;
+    Uri get_file_uri;
     Intent fileIntent;
+
+    String save_text = null;
+    String save_filename;
 
     StringBuilder builder = new StringBuilder();
 
     //Class
     DataGeneration datepars = new DataGeneration();
     OpenFile openFile = new OpenFile();
+    SaveFile saveFile = new SaveFile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coding);
 
         opentxt = (Button) findViewById(R.id.coding_open_txt);
         savetxt = (Button) findViewById(R.id.coding_save_txt);
         edit_text = (EditText) findViewById(R.id.editview_coding);
-
     }
 
 
@@ -68,23 +74,21 @@ public class Coding extends AppCompatActivity
         fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
         fileIntent.setType("*/*");
         startActivityForResult(fileIntent,5);
-
     }
 
     public void SaveTextToFile()
     {
 
-        int WRITE_REQUEST_CODE = 43;
         String mimeType ="text/plain";
-        String fileName = "code.result."+datepars.save_date()+".txt";
+        save_filename = "code.result."+datepars.save_date()+".txt";
 
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
 
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType(mimeType);
 
-        intent.putExtra(Intent.EXTRA_TITLE, fileName);
-        startActivityForResult(intent, WRITE_REQUEST_CODE);
+        intent.putExtra(Intent.EXTRA_TITLE, save_filename);
+        startActivityForResult(intent, 4);
 
     }
 
@@ -97,9 +101,16 @@ public class Coding extends AppCompatActivity
             {
                 if(resultCode==RESULT_OK)
                 {
-                    op_file_uri = data.getData();
-                    edit_text.setText(builder = openFile.copy_to_editview(op_file_uri,getApplicationContext()));
+                    get_file_uri = data.getData();
+                    edit_text.setText(builder = openFile.copy_to_editview(get_file_uri,getApplicationContext()));
                 }
+             break;
+            }
+            case 4:
+            {
+                get_file_uri = data.getData();
+                save_text=edit_text.getText().toString();
+                saveFile.writeToFile(save_text,getApplicationContext(),save_filename,get_file_uri);
              break;
             }
         }
